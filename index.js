47f250912ed7efffa141677dacf48ac59346e75e
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2025-03-13 21:17:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ const server = express()
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 const wss = new Server({ server });
 
+app.get('/', (req, res) => {
+  console.log(`HTTP HIT: ${new Date().toISOString()} - IP: ${req.ip}`);
+  res.send('<html><body><div>CoreBoys</div></body></html>');
+});
 
 let boys = []
 let games = []
